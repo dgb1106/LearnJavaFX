1b112789d2c6aeda990e0ccf21f38a2663b45102
dgb1106 <duonggiabao1106@gmail.com>
2024-10-03 17:31:58 +0700

    learned about stage

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -1,6 +1,7 @@
 module org.example.learnjavafx {
     requires javafx.controls;
     requires javafx.fxml;
+    requires java.desktop;
 
 
     opens org.example.learnjavafx to javafx.fxml;
--- src/main/java/org/example/learnjavafx/HelloApplication.java
+++ src/main/java/org/example/learnjavafx/HelloApplication.java
@@ -2,22 +2,41 @@ package org.example.learnjavafx;
 
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
+import javafx.scene.Group;
 import javafx.scene.Scene;
+import javafx.scene.input.KeyCombination;
 import javafx.stage.Stage;
+import javafx.scene.paint.Color;
+import javafx.scene.image.Image;
 
+import java.awt.*;
 import java.io.IOException;
 
 public class HelloApplication extends Application {
-    @Override
-    public void start(Stage stage) throws IOException {
-        FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("hello-view.fxml"));
-        Scene scene = new Scene(fxmlLoader.load(), 320, 240);
-        stage.setTitle("Hello!");
-        stage.setScene(scene);
-        stage.show();
-    }
 
     public static void main(String[] args) {
-        launch();
+        launch(args);
+    }
+
+    @Override
+    public void start(Stage stage) throws Exception {
+        // Stage stage = new Stage();
+        Group root = new Group();
+        Scene scene = new Scene(root, Color.BLACK);
+
+        Image icon = new Image(getClass().getResourceAsStream("/icon.jpg"));
+        stage.getIcons().add(icon);
+        stage.setTitle("Stage Demo Program");
+        stage.setWidth(800);
+        stage.setHeight(600);
+        stage.setResizable(false);
+//        stage.setX(50);
+//        stage.setY(50);
+        stage.setFullScreen(true);
+        stage.setFullScreenExitHint("YOU CAN'T ESCAPE unless you press Q");
+        stage.setFullScreenExitKeyCombination(KeyCombination.valueOf("q"));
+
+        stage.setScene(scene);
+        stage.show();
     }
 }
