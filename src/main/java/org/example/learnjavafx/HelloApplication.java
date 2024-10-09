b9548c2d0f1f5eb941231d2c712d4f0e355d26a6
dgb1106 <duonggiabao1106@gmail.com>
2024-10-10 05:19:55 +0700

    shapes and images

--- src/main/java/org/example/learnjavafx/HelloApplication.java
+++ src/main/java/org/example/learnjavafx/HelloApplication.java
@@ -13,9 +13,14 @@ import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
 
 import javafx.scene.shape.Line;
+import javafx.scene.shape.Rectangle;
+import javafx.scene.shape.Shape;
+import javafx.scene.shape.Polygon;
+import javafx.scene.shape.Circle;
 
 //import javax.sound.sampled.Line;
-import java.awt.*;
+
+import java.awt.color.*;
 import java.io.IOException;
 
 public class HelloApplication extends Application {
@@ -27,23 +32,23 @@ public class HelloApplication extends Application {
     @Override
     public void start(Stage primaryStage) throws Exception {
         // Stage stage = new Stage();
-//        Group root = new Group();
-//        Scene scene = new Scene(root, Color.BLACK);
-//
-//        Image icon = new Image(getClass().getResourceAsStream("/icon.jpg"));
-//        stage.getIcons().add(icon);
-//        stage.setTitle("Stage Demo Program");
-//        stage.setWidth(800);
-//        stage.setHeight(600);
-//        stage.setResizable(false);
-//        stage.setX(50);
-//       stage.setY(50);
-//        stage.setFullScreen(true);
-//        stage.setFullScreenExitHint("YOU CAN'T ESCAPE unless you press Q");
-//        stage.setFullScreenExitKeyCombination(KeyCombination.valueOf("q"));
-//
-//        stage.setScene(scene);
-//        stage.show();
+        // Group root = new Group();
+        // Scene scene = new Scene(root, Color.BLACK);
+
+        // Image icon = new Image(getClass().getResourceAsStream("/icon.jpg"));
+        // stage.getIcons().add(icon);
+        // stage.setTitle("Stage Demo Program");
+        // stage.setWidth(800);
+        // stage.setHeight(600);
+        // stage.setResizable(false);
+        // stage.setX(50);
+        // stage.setY(50);
+        // stage.setFullScreen(true);
+        // stage.setFullScreenExitHint("YOU CAN'T ESCAPE unless you press Q");
+        // stage.setFullScreenExitKeyCombination(KeyCombination.valueOf("q"));
+
+        // stage.setScene(scene);
+        // stage.show();
 
         Group root = new Group();
         Scene scene = new Scene(root, 600, 600, Color.LIGHTSKYBLUE);
@@ -58,8 +63,44 @@ public class HelloApplication extends Application {
 
         Line line = new Line();
         line.setStartX(200);
+        line.setStartY(200);
+        line.setEndX(500);
+        line.setEndY(200);
+        line.setStrokeWidth(5);
+        line.setStroke(Color.RED);
+        line.setOpacity(0.5);
+        line.setRotate(45);
+        
+        Rectangle rectangle = new Rectangle();
+        rectangle.setX(100);
+        rectangle.setY(100);
+        rectangle.setWidth(100);
+        rectangle.setHeight(100);
+        rectangle.setFill(Color.BLUE);
+        rectangle.setStrokeWidth(5);
+        rectangle.setStroke(Color.BLACK);
+
+        Polygon triangle = new Polygon();
+        triangle.getPoints().setAll(200.0, 200.0, 300.0, 300.0, 200.0, 300.0);
+        triangle.setFill(Color.YELLOW);
+
+        Circle circle = new Circle();
+        circle.setCenterX(350);
+        circle.setCenterY(350);
+        circle.setRadius(50);
+        circle.setFill(Color.ORANGE);
+
+        Image image = new Image("/icon.jpg");
+        ImageView imageView = new ImageView(image);
+        imageView.setX(400);
+        imageView.setY(400);
 
         root.getChildren().add(text);
+        root.getChildren().add(line);
+        root.getChildren().add(rectangle);
+        root.getChildren().add(triangle);
+        root.getChildren().add(circle);
+        root.getChildren().add(imageView);
         stage.setScene(scene);
         stage.show();
     }
